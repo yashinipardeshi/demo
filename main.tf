e980a939604a3c57c846969cb3f164a3af97ccd6
Yashini <yashini.pardeshi@coditas.com>
2026-04-19 16:37:01 +0530

    Remove hardcoded Azure storage key, use variable instead

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
     key                  = "terraform.tfstate"
     use_azuread_auth     = true
     use_oidc             = true
-    access_key           = "REDACTED"
+    access_key           = var.storage_access_key
     subscription_id      = "9919455d-a384-407d-849e-190bad4af9d7"
     tenant_id            = "550d7d5c-247a-472e-af33-9915c948872b"
     resource_group_name  = "stcapp-rg"
